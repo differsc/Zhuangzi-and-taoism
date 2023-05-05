df01cab553d6f236844074a9b136d0a3743ca1a3
djbpitt <djbpitt@gmail.com>
2023-05-05 15:45:11 -0400

    Reorder if/else to show/hide checkboxes

--- html/corpus.js
+++ html/corpus.js
@@ -1,15 +1,18 @@
-
+"use strict";
 function init () {
-    const button = document.getElementById("button");
-   button.addEventListener('click', open);
-    }
-
+  const button = document.getElementById("button");
+  button.addEventListener('click', open);
+}
 function open () {
-    var content = document.querySelector(".toolbar");
-    if (content.style.display === "block") {
-        content.style.display = "none";
-    } else {
-        content.style.display = "block";
-    }
-};
+  // Three states: 1) explicit "block", 2) explicit "none", or
+  //   3) nothing specified, which is implicit "block"
+  // Test for "none" so that block plus nothing are treated
+  //   the same way as the "else" condition
+  var content = document.querySelector(".toolbar");
+  if (content.style.display === "none") {
+    content.style.display = "block";
+  } else {
+    content.style.display = "none";
+  }
+}
 document.addEventListener('DOMContentLoaded', init);
